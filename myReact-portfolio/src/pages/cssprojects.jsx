5b70e32b1d331bca9f5815da948778e1a64d3791
Azizullah <azizullahcodes@gmail.com>
2026-01-03 09:49:00 +0500

    css-section progress

--- myReact-portfolio/src/pages/cssprojects.jsx
+++ myReact-portfolio/src/pages/cssprojects.jsx
@@ -2,9 +2,19 @@
 import React from "react";
 const CssProjects = ()=>{
     return(
-        <>
-        <h1>CssProjects page</h1>
-        </>
+       <section>
+
+        {/* heading */}
+        <h2>My Css Projects</h2>
+        {/* short description */}
+        <p></p>
+        {/* Projects Grid */}
+        <div>
+            {CssProjects.map((projects)=>{
+             <div></div>
+            })}
+        </div>
+       </section>
     )
 }
 export default CssProjects;
