f163dd4247ed57220aa80a11fbe5f233ae2c4f6c
Azizullah <azizullahcodes@gmail.com>
2026-01-08 11:23:21 +0500

    upd

--- myReact-portfolio/src/App.jsx
+++ myReact-portfolio/src/App.jsx
@@ -3,12 +3,17 @@ import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/approutes';
 import Navbar from './components/navbar';
+import Home from './pages/home';
+import About from './pages/about';
+import CssProjects from './pages/cssprojects';
+import JavascriptProjects from './pages/javascriptprojects';
 function App() {
 
   return (
    
     <BrowserRouter>
 <Navbar/>
+
    <AppRoutes/>
       </BrowserRouter>
 
